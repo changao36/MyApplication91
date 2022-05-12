18936b2f3a19cfbce989c6c7d0f6c3b2d61c3f00
changao36 <changao36@163.com>
2022-05-12 14:05:18 +0800

    test9

--- app/src/main/java/com/test/myapplication91/ListenerActivity.java
+++ app/src/main/java/com/test/myapplication91/ListenerActivity.java
@@ -8,27 +8,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
+
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
+
 
 public class ListenerActivity extends Activity {
 
-    private TextView textView0,textView1,textView2,textView3;
+    private TextView textView0, textView1, textView2, textView3;
     private Button button1;
     private LinearLayout linearLayout;
     String maxMemory = "";
-    int x = 0;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,9 +42,9 @@ public class ListenerActivity extends Activity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (getScreenBrightness(ListenerActivity.this)==15 && getNetworkState()==true){
+                if (getScreenBrightness(ListenerActivity.this) == 15 && getNetworkState() == true) {
                     Toast.makeText(ListenerActivity.this, "系统设置与预设值一样", Toast.LENGTH_SHORT).show();
-                }else {
+                } else {
                     Toast.makeText(ListenerActivity.this, "系统设置发生改变", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -62,16 +58,17 @@ public class ListenerActivity extends Activity {
             }
         });
     }
-    public void click(){
+
+    public void click() {
         ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
         ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
         activityManager.getMemoryInfo(memoryInfo);
-        float maxMemory1 = (float) (Runtime.getRuntime().maxMemory() * 1.0/ (1024 * 1024));
-        textView1.setText("最大分配内存为:"+maxMemory1);
-        float totalMemory = (float) (Runtime.getRuntime().totalMemory() * 1.0/ (1024 * 1024));
-        textView2.setText("当前分配的总内存为:"+totalMemory);
-        float freeMemory = (float) (Runtime.getRuntime().freeMemory() * 1.0/ (1024 * 1024));
-        textView3.setText("剩余内存为:"+freeMemory);
+        float maxMemory1 = (float) (Runtime.getRuntime().maxMemory() * 1.0 / (1024 * 1024));
+        textView1.setText("最大分配内存为:" + maxMemory1);
+        float totalMemory = (float) (Runtime.getRuntime().totalMemory() * 1.0 / (1024 * 1024));
+        textView2.setText("当前分配的总内存为:" + totalMemory);
+        float freeMemory = (float) (Runtime.getRuntime().freeMemory() * 1.0 / (1024 * 1024));
+        textView3.setText("剩余内存为:" + freeMemory);
         TextView textView = new TextView(ListenerActivity.this);
         textView.setText("系统剩余内存为：" + String.valueOf(memoryInfo.availMem / (1024 * 1024)) + "MB");
         linearLayout.addView(textView);
@@ -90,7 +87,7 @@ public class ListenerActivity extends Activity {
         return value;
     }
 
-    public boolean getNetworkState(){
+    public boolean getNetworkState() {
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);//使用getSystemService得到ConnectivityManager实例
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         return networkInfo.isAvailable();
